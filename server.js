fd2a805ea1865b854a8d7fee74b1a335e39573b3
Elijah Davis <elijahgdavis1999@gmail.com>
2021-01-26 14:57:15 -0800

    Finished setup for server.js

--- server.js
+++ server.js
@@ -1,3 +1,20 @@
-const express = require("express");
-var orm = require("./config/orm.js");
+const express = require('express');
+const bodyParser = require("body-parser");
+const exphbs = require("express-handlebars");
+const app = express();
+const PORT = process.env.PORT || 3000;
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
+// Add Routes
+app.use(express.static(__dirname + '/public'));
+app.use('/', require('./controllers/burgersController'));
+
+// Set up handlebars view engine
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+app.listen(PORT, () =>
+  console.log(`Listening on ${PORT}`)
+);
