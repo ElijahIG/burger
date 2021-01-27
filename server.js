9cc462fcb71912574069021e556316b82dd02e72
Elijah Davis <elijahgdavis1999@gmail.com>
2021-01-26 17:02:35 -0800

    fixed style sheet link and removed __dirname

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 
 // Add Routes
-app.use(express.static(__dirname + '/public'));
+app.use(express.static('public'));
 app.use('/', require('./controllers/burgersController'));
 
 // Set up handlebars view engine
